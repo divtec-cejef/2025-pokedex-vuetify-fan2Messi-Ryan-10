c0af68bd6a3d3bfe8f43c8733816a3571c23fe5d
Ryan Berthoud <ryan.berthoud@divtec.ch>
2025-09-29 18:41:08 +0200

    Etape 5 - Pokédex: fin de la préparation à l'affichage de la liste des pokémons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,81 @@
 <template>
-  <!-- Conteneur principal pour structurer la disposition de la page -->
   <v-container>
-    <!--
-    Titre de la page
-      * mb-6 permet d'ajouter une Marge en Bas de 6 unités
-      * text-center permet de centrer le texte
-    -->
-    <h1 class="mb-6 text-center">
-      Page d'accueil Pokédex
-    </h1>
+    <h1 class="mb-6 text-center">Pokédex</h1>
+
+    <v-text-field
+      label="Rechercher un Pokémon"
+      prepend-icon="mdi-magnify"
+      clearable
+    />
+
+    <v-row>
+      <!-- Exemple de multiples colonnes avec du contenu statique pour tester l'affichage -->
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <v-card>
+          <v-card-text>Carte à venir...</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <v-card>
+          <v-card-text>Carte à venir...</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <v-card>
+          <v-card-text>Carte à venir...</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <v-card>
+          <v-card-text>Carte à venir...</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <v-card>
+          <v-card-text>Carte à venir...</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <v-card>
+          <v-card-text>Carte à venir...</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
-
-<script setup>
-// Vos scripts ou imports ici
-</script>
